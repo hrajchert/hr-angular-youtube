342d32cded0465b1bb45f98052118bc8eac76d49
Hernan Rajchert <hrajchert@gmail.com>
2014-06-28 21:33:14 -0300

    Added minified js and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    prefix = require('gulp-autoprefixer');
+    prefix = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
 
 gulp.task('process-scripts', function() {
     gulp.src('./src/**/*.js')
         .pipe(concat('hr-angular-youtube.js'))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/'));
+
 });
 
 gulp.task('process-styles', function() {
     gulp.src('./assets/**/*.css')
         .pipe(concat('hr-angular-youtube.css'))
         .pipe(prefix('last 2 version'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist'))
+        .pipe(minifycss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('watch', function() {
