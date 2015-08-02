6965f404859cd2c9189befd7870e24b91345e7b2
Hernan Rajchert <hrajchert@gmail.com>
2015-08-02 00:12:56 -0300

    Small refactor in youtube quality map

--- src/service/youtube-quality-map.js
+++ src/service/youtube-quality-map.js
@@ -1,10 +1,21 @@
-/* global angular */
-(function(angular) {
+(function() {
+    'use strict';
 
+    angular
+        .module('hrAngularYoutube')
+        .factory('youtubeQualityMap', youtubeQualityMapService);
 
-    angular.module('hrAngularYoutube')
+    /**
+     * @ngdoc service
+     * @name hrAngularYoutube.factory:youtubeQualityMap
+     *
+     * @description
+     * TODO: Describe this service
+     *
+     */
+    youtubeQualityMapService.$inject = [];
 
-    .factory('youtubeQualityMap', function () {
+    function youtubeQualityMapService () {
         var map = {
             'hd1080' : '1080p',
             'hd720' : '720p',
@@ -14,11 +25,26 @@
             'tiny' : '144p',
             'auto' : 'Auto'
         };
+
         var inverseMap = {};
-        var inverse;
-        for (var q in map) {
-            inverse = map[q];
-            inverseMap[inverse] = q;
+
+        initialize();
+
+        return {
+            convertToYoutube: convertToYoutube,
+            convertFromYoutube: convertFromYoutube,
+            convertToYoutubeArray: convertToYoutubeArray
+        };
+
+        ///////////////////////////////////////////
+
+        function initialize () {
+            var inverse;
+            for (var q in map) {
+                inverse = map[q];
+                inverseMap[inverse] = q;
+            }
+
         }
 
         function _doConvertToYoutube(q) {
@@ -28,26 +54,50 @@
             }
             return ans;
         }
-        return {
-            convertToYoutube : function (q) {
-                return _doConvertToYoutube(q);
-            },
-            convertFromYoutube: function (q) {
-                var ans = inverseMap[q];
-                if (!ans) {
-                    ans = 'default';
-                }
-                return ans;
-            },
-            convertToYoutubeArray : function (arr) {
-                var ans = [];
-                for (var i = 0; i<arr.length; i++) {
-                    ans.push(_doConvertToYoutube(arr[i]));
-                }
-                return ans;
+
+        /**
+         * @ngdoc method
+         * @name convertToYoutube
+         * @methodOf hrAngularYoutube.factory:youtubeQualityMap
+         *
+         * @description
+         * TODO: convertToYoutube description
+        */
+        function convertToYoutube (q) {
+            return _doConvertToYoutube(q);
+        }
+
+        /**
+         * @ngdoc method
+         * @name convertFromYoutube
+         * @methodOf hrAngularYoutube.factory:youtubeQualityMap
+         *
+         * @description
+         * TODO: convertFromYoutube description
+        */
+        function convertFromYoutube (q) {
+            var ans = inverseMap[q];
+            if (!ans) {
+                ans = 'default';
             }
-        };
-    });
+            return ans;
+        }
 
+        /**
+         * @ngdoc method
+         * @name convertToYoutubeArray
+         * @methodOf hrAngularYoutube.factory:youtubeQualityMap
+         *
+         * @description
+         * TODO: convertToYoutubeArray description
+        */
 
-})(angular);
+        function convertToYoutubeArray (arr) {
+            var ans = [];
+            for (var i = 0; i<arr.length; i++) {
+                ans.push(_doConvertToYoutube(arr[i]));
+            }
+            return ans;
+        }
+    }
+})();
